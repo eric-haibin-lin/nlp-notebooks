907c3986abed2ada0ce36f99168b159aed079981
Haibin Lin <linhaibin.eric@gmail.com>
2019-07-02 21:57:10 -0700

    Add util file

---
+++ gluon_basics/mlp_utils.py
@@ -0,0 +1,10 @@
+def show_fashion_mnist(images, labels):
+    import d2l
+    d2l.use_svg_display()
+    # Here _ means that we ignore (not use) variables.
+    _, figs = d2l.plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.reshape((28, 28)).asnumpy())
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
